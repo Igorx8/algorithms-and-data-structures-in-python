944686b1f63bd3dbc298b86433deb6d148e447b2
igorx8 <igor.carvalhox8@gmail.com>
2025-01-10 15:11:06 -0300

    insertion sort

---
+++ sort-algorithms/insertion-sort.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def insertion_sort(arr):
+    n = len(arr)
+
+    for i in range(1, n):
+        target = arr[i]
+        
+        j = i - 1
+
+        while j>=0 and target < arr[j]:
+            arr[j + 1] = arr[j]
+            j-=1
+        arr[j + 1] = target
+
+    print(arr)
+    return arr
+
+
+insertion_sort(np.array([15,3,25,1,13], dtype=int))
