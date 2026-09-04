fe52b4321ed73f7de40d7d058de3fdb45688b52e
Igor Carvalho <igor.carvalhox8@gmail.com>
2026-09-04 16:15:12 -0300

    Add find method to binary search tree for value retrieval

--- data-structure/python/binary-tree/bsearch_tree.py
+++ data-structure/python/binary-tree/bsearch_tree.py
@@ -25,6 +25,21 @@ class BSearchTree:
                         parent.right = new
                         return
 
+    def find(self, value: int):
+        current = self.root
+
+        while current is not None:
+            if value == current.value:
+                print(value)
+                return current.value
+            elif value < current.value:
+                current = current.left
+            else:
+                current = current.right
+
+        print("Not found")
+        return False
+
 
 bst = BSearchTree()
 bst.insert(3)
@@ -32,3 +47,9 @@ bst.insert(2)
 bst.insert(5)
 bst.insert(4)
 bst.insert(1)
+bst.find(3)
+bst.find(6)
+bst.find(0)
+bst.find(1)
+bst.find(5)
+bst.find(4)
